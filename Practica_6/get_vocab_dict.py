2bae4ff6381e785f615413eaaf34b7aafbff7a07
Arturo <arbarber@ucm.es>
2018-12-25 18:15:27 +0100

    Hasta el 1.2 Kernel Gaussiano

---
+++ Practica_6/get_vocab_dict.py
@@ -0,0 +1,16 @@
+def getVocabDict(reverse=False):
+    """
+    Function to read in the supplied vocab list text file into a dictionary.
+    Dictionary key is the stemmed word, value is the index in the text file
+    If "reverse", the keys and values are switched.
+    """
+    vocab_dict = {}
+    with open("vocab.txt") as f:
+        for line in f:
+            (val, key) = line.split()
+            if not reverse:
+                vocab_dict[key] = int(val)
+            else:
+                vocab_dict[int(val)] = key
+
+    return vocab_dict
---
+++ Practica_6/process_email.py
@@ -0,0 +1,51 @@
+import re
+import nltk
+import nltk.stem.porter
+
+
+def preProcess(email):
+    
+    hdrstart = email.find("\n\n")
+    if hdrstart != -1:
+        email = email[hdrstart:]
+
+    email = email.lower()
+    # Strip html tags. replace with a space
+    email = re.sub('<[^<>]+>', ' ', email)
+    # Any numbers get replaced with the string 'number'
+    email = re.sub('[0-9]+', 'number', email)
+    # Anything starting with http or https:// replaced with 'httpaddr'
+    email = re.sub('(http|https)://[^\s]*', 'httpaddr', email)
+    # Strings with "@" in the middle are considered emails --> 'emailaddr'
+    email = re.sub('[^\s]+@[^\s]+', 'emailaddr', email)
+    # The '$' sign gets replaced with 'dollar'
+    email = re.sub('[$]+', 'dollar', email)
+    return email
+
+
+def email2TokenList(raw_email):
+    """
+    Function that takes in a raw email, preprocesses it, tokenizes it,
+    stems each word, and returns a list of tokens in the e-mail
+    """
+
+    stemmer = nltk.stem.porter.PorterStemmer()
+    email = preProcess(raw_email)
+
+    # Split the e-mail into individual words (tokens) 
+    tokens = re.split('[ \@\$\/\#\.\-\:\&\*\+\=\[\]\?\!\(\)\{\}\,\'\"\>\_\<\;\%]',
+                      email)
+
+    # Loop over each token and use a stemmer to shorten it
+    tokenlist = []
+    for token in tokens:
+
+        token = re.sub('[^a-zA-Z0-9]', '', token)
+        stemmed = stemmer.stem(token)
+        #Throw out empty tokens
+        if not len(token):
+            continue
+        # Store a list of all unique stemmed words
+        tokenlist.append(stemmed)
+
+    return tokenlist
